49fac421ae2684361939190409c77835c44c716f
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-26 05:48:09 +0200

    Add footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Google clone",
-  description: "An open source google clone built with NextJS 14 and TailwindCSS",
+  description:
+    "An open source google clone built with NextJS 14 and TailwindCSS",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
