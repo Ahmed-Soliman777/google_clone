7e1ee1b75166f083940cc274420d5a45a7802200
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-27 06:26:08 +0200

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,11 +1,30 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CTX_KEY}&q=${searchParams.searchTerm}`
   );
+  if (!response.ok) {
+    throw new Error("Something went wrong")
+  }
   const data = await response.json()
   const results = data.items
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className='text-3xl mb-4'>
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching for something else
+        </p>
+        <Link href='/' className='text-blue-500'>
+          Home
+        </Link>
+      </div>
+    )
+  }
   return (
     <div>
       {results && results.map((result) => (
